959eadd06512b46a7350d6e4d047b422cdb6437a
stilian-iliev <74832022+stilian-iliev@users.noreply.github.com>
2022-04-03 17:40:29 +0300

    ship is movable

--- src/game.js
+++ src/game.js
@@ -1,40 +1,47 @@
-import { updateShip } from "./ship.js";
+import * as ship from "./ship.js";
 
 export const FPS = 30; // frames per second
 export const canv = document.getElementById("gameCanvas");
 export const ctx = canv.getContext("2d");
 
-// set up event handlers
+window.addEventListener('resize', resize)
 document.addEventListener("keydown", keyDown);
 document.addEventListener("keyup", keyUp);
+
+function resize(){
+    canv.width = window.innerWidth;
+    canv.height = window.innerHeight;
+}
+  
+resize()
 
 // set up the game loop
 setInterval(update, 1000 / FPS);
 
-function keyDown(/** @type {KeyboardEvent} */ event) {
+function keyDown(event) {
     switch(event.keyCode) {
         case 37: // left arrow (rotate ship left) +
-            
+            ship.rotateLeft();
             break;
         case 38: // up arrow (thrust the ship forward)
-            
+            ship.thrust();
             break;
         case 39: // right arrow (rotate ship right) -
-            
+            ship.rotateRight();
             break;
     }
 }
 
-function keyUp(/** @type {KeyboardEvent} */ event) {
+function keyUp(event) {
     switch(event.keyCode) {
-        case 37: // left arrow (stop rotating left)
-            
+        case 37: // left arrow 
+            ship.stopRotation();
             break;
-        case 38: // up arrow (stop thrusting)
-            
+        case 38: // up arrow
+            ship.stopThrust();
             break;
-        case 39: // right arrow (stop rotating right)
-            
+        case 39: // right arrow
+            ship.stopRotation();
             break;
     }
 }
@@ -43,8 +50,9 @@ function update() {
     // draw space
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canv.width, canv.height);
-
-    updateShip();
+    
+    ship.update();
     // thrust the ship
 
 }
+
--- src/ship.js
+++ src/ship.js
@@ -2,8 +2,9 @@ import { canv, ctx, FPS } from "./game.js";
 
 const FRICTION = 0.7; // friction coefficient of space (0 = no friction, 1 = lots of friction)
 const SHIP_SIZE = 30; // ship height in pixels
-const SHIP_THRUST = 5; // acceleration of the ship in pixels per second per second
+const SHIP_THRUST = 10; // acceleration of the ship in pixels per second per second
 const TURN_SPEED = 360; // turn speed in degrees per second
+const SPEED_LIMIT = 50; // turn speed in degrees per second
 
 const ship = {
     x: canv.width / 2,
@@ -18,6 +19,68 @@ const ship = {
     }
 }
 
-export function updateShip() {
-        
+export function update() {
+    //draw ship
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = SHIP_SIZE / 20;
+    ctx.beginPath();
+    ctx.moveTo( // nose of the ship
+        ship.x + 4 / 3 * ship.r * Math.cos(ship.a),
+        ship.y - 4 / 3 * ship.r * Math.sin(ship.a)
+    );
+    ctx.lineTo( // rear left
+        ship.x - ship.r * (2 / 3 * Math.cos(ship.a) + Math.sin(ship.a)),
+        ship.y + ship.r * (2 / 3 * Math.sin(ship.a) - Math.cos(ship.a))
+    );
+    ctx.lineTo( // rear right
+        ship.x - ship.r * (2 / 3 * Math.cos(ship.a) - Math.sin(ship.a)),
+        ship.y + ship.r * (2 / 3 * Math.sin(ship.a) + Math.cos(ship.a))
+    );
+    ctx.closePath();
+    ctx.stroke();
+
+    const velocity = Math.sqrt(Math.pow(ship.thrust.x, 2) + Math.pow(ship.thrust.y, 2));
+    if (ship.thrusting && velocity < SPEED_LIMIT) {
+        ship.thrust.x += SHIP_THRUST * Math.cos(ship.a) / FPS;
+        ship.thrust.y -= SHIP_THRUST * Math.sin(ship.a) / FPS;
+    } else {
+        ship.thrust.x -= FRICTION * ship.thrust.x / FPS;
+        ship.thrust.y -= FRICTION * ship.thrust.y / FPS;
+    }
+
+    ship.a += ship.rot;
+
+    ship.x += ship.thrust.x;
+    ship.y += ship.thrust.y;
+
+    if (ship.x + ship.r < 0) {
+        ship.x = canv.width + ship.r;
+    } else if(ship.x - ship.r > canv.width) {
+        ship.x = 0 - ship.r;
+    }
+    if (ship.y + ship.r < 0) {
+        ship.y = canv.height + ship.r;
+    } else if(ship.y - ship.r > canv.height) {
+        ship.y = 0 - ship.r;
+    }
+}
+
+export function rotateLeft() {
+    ship.rot = TURN_SPEED / 180 * Math.PI / FPS;
+}
+
+export function rotateRight() {
+    ship.rot = -TURN_SPEED / 180 * Math.PI / FPS;
+}
+
+export function thrust() {
+    ship.thrusting = true;
+}
+
+export function stopRotation() {
+    ship.rot = 0;
+}
+
+export function stopThrust() {
+    ship.thrusting = false;
 }
